b2665398368c3e45920e8ae583538c08491a03c1
Nico-app <nicocamara92@gmail.com>
2022-06-28 14:07:34 -0300

    agregar archivos

---
+++ src/components/Emty.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./Emty.module.css";
+
+export default function Emty() {
+  return (
+    <div className={styles.emty}>
+      No Results
+    </div>
+  )
+}
